6cb20e03f20670dc884bb66319456cf7d96b486a
Bartlomiej Ciurla <external.bartlomiej.ciurla@bcc.no>
2020-01-30 15:26:18 +0100

    initial commit

---
+++ module1.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonHeaderComponent } from './module1/common-header/common-header.component';
+
+@NgModule({
+  declarations: [
+  CommonHeaderComponent],
+  imports: [
+  ],
+  exports: [
+    CommonHeaderComponent
+  ],
+  providers: [],
+  bootstrap: []
+})
+export class Module1 { }
---
+++ module1/common-header/common-header.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonHeaderComponent } from './common-header.component';
+
+@NgModule({
+  declarations: [CommonHeaderComponent],
+  imports: [ ],
+  exports: [CommonHeaderComponent
+  ],
+  providers: [],
+  bootstrap: []
+})
+export class CommonHeaderModule { }
